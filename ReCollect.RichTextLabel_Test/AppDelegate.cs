bf42f86fc6565b2e62c1a6ad3f16c610b42f3894
Kevin Jones <kvn.jns@gmail.com>
2015-01-19 15:16:19 -0500

    s/HtmlLabel/RichTextLabel/g

--- ReCollect.RichTextLabel_Test/AppDelegate.cs
+++ ReCollect.RichTextLabel_Test/AppDelegate.cs
@@ -6,7 +6,7 @@ using Foundation;
 using UIKit;
 using ReCollect;
 
-namespace ReCollect.HtmlLabel_Test
+namespace ReCollect.RichTextText_Test
 {
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to
--- ReCollect.RichTextLabel_Test/Main.cs
+++ ReCollect.RichTextLabel_Test/Main.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
-namespace ReCollect.HtmlLabel_Test
+namespace ReCollect.RichTextText_Test
 {
 	public class Application
 	{
